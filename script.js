22a3401fac675eb2e859e317a99bd53c48e8240f
Letícia <leticia.c.rodrigues0@gmail.com>
2024-02-06 21:47:30 -0300

    html e css

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let active = 'horarios';
 
 const horariosPage = document.querySelector("#horarios");
-const avisosPage = document.querySelector("#avisos");
+const avisosPage = document.querySelector("#notices");
 
-setInterval(() => {
-    if (active == 'horarios'){
-        horariosPage.className = "page"
-        avisosPage.className += " hidden";
-        active = 'avisos';
-    } else {
-        avisosPage.className = "page"
-        horariosPage.className += " hidden";
-        active = 'horarios';
-    }
+// setInterval(() => {
+//     if (active == 'horarios'){
+//         horariosPage.className = "page"
+//         avisosPage.className += " hidden";
+//         active = 'avisos';
+//     } else {
+//         avisosPage.className = "page"
+//         horariosPage.className += " hidden";
+//         active = 'horarios';
+//     }
 
-}, 6000)
+// }, 600);
