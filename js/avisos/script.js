a5d8ee67ef1a9196d5e1b01a810262de2b5f5c7c
Letícia <leticia.c.rodrigues0@gmail.com>
2024-02-11 18:44:32 -0300

    some css fixes

--- js/avisos/script.js
+++ js/avisos/script.js
@@ -3,14 +3,14 @@ let active = 'horarios';
 const horariosPage = document.querySelector("#horarios");
 const avisosPage = document.querySelector("#notices");
 
-setInterval(() => {
-    if (active == 'horarios'){
-        horariosPage.className = "page-horarios"
-        avisosPage.className += " hidden";
-        active = 'avisos';
-    } else {
-        avisosPage.className = "page-notice"
-        horariosPage.className += " hidden";
-        active = 'horarios';
-    }
-}, 600);
+// setInterval(() => {
+//     if (active == 'horarios'){
+//         horariosPage.className = "page-horarios"
+//         avisosPage.className += " hidden";
+//         active = 'avisos';
+//     } else {
+//         avisosPage.className = "page-notice"
+//         horariosPage.className += " hidden";
+//         active = 'horarios';
+//     }
+// }, 600);
